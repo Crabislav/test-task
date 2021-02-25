2d57cd867163832eb1fe198b4a51e66da9098422
Alex Kopnin <alex.kopnin2@gmail.com>
2021-02-25 10:53:33 +0200

    [SymbolRepository]: remove unused import

--- src/main/java/com/ak/testtask/repositories/SymbolRepository.java
+++ src/main/java/com/ak/testtask/repositories/SymbolRepository.java
@@ -2,10 +2,7 @@ package com.ak.testtask.repositories;
 
 import com.ak.testtask.model.Symbol;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface SymbolRepository extends JpaRepository<Symbol, Long> {
